c53668b63d8cf1bdf774e931c65d392cb916a75e
Hywfred <hitwhy@gmail.com>
2020-02-19 17:33:23 +0800

    add algorithm roman to integer

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 
-	"github.com/Hywfred/go-for-leetcode/leetcode/integertoroman"
+	"github.com/Hywfred/go-for-leetcode/leetcode/romantointeger"
 )
 
 func main() {
-	fmt.Println(integertoroman.IntToRoman(20))
+	fmt.Println(romantointeger.RomanToInt("III"))
+	fmt.Println(romantointeger.RomanToInt("IV"))
+	fmt.Println(romantointeger.RomanToInt("IX"))
+	fmt.Println(romantointeger.RomanToInt("LVIII"))
+	fmt.Println(romantointeger.RomanToInt("MCMXCIV"))
 }
