e317bfc6ba40af0f5ae9e524d4cb28a34b786381
Hywfred <hitwhy@gmail.com>
2020-02-28 14:48:25 +0800

    add algorithm rotate image

--- main.go
+++ main.go
@@ -3,20 +3,21 @@ package main
 import (
 	"fmt"
 
-	"github.com/Hywfred/go-for-leetcode/leetcode/array/middle/combinationSum"
+	"github.com/Hywfred/go-for-leetcode/leetcode/array/middle/rotateImage"
 )
 
 func main() {
-	nums := []int{2, 3, 5}
-	fmt.Printf("%v\n", combinationSum.CombinationSum(nums, 8))
-	nums = []int{2, 3, 6, 7}
-	fmt.Printf("%v\n", combinationSum.CombinationSum(nums, 7))
-	nums = []int{2}
-	fmt.Printf("%v\n", combinationSum.CombinationSum(nums, 1))
-	nums = []int{}
-	fmt.Printf("%v\n", combinationSum.CombinationSum(nums, 7))
-	nums = []int{1, 2}
-	fmt.Printf("%v\n", combinationSum.CombinationSum(nums, 1))
-	nums = []int{7, 3, 2}
-	fmt.Printf("%v\n", combinationSum.CombinationSum(nums, 18))
+	nums := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	rotateImage.Rotate(nums)
+	fmt.Printf("%v\n", nums)
+	nums = [][]int{
+		{1, 2},
+		{3, 4},
+	}
+	rotateImage.Rotate(nums)
+	fmt.Printf("%v\n", nums)
 }
