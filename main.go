f9e047b1b9b88b39a33796b7e6787d18646807f0
Hywfred <hitwhy@gmail.com>
2020-03-05 09:34:05 +0800

    add algorithm distribute candies

--- main.go
+++ main.go
@@ -7,24 +7,7 @@ import (
 )
 
 func main() {
-	src := [][]int{
-		{2, 1, 1},
-		{1, 1, 0},
-		{0, 1, 1},
-	}
-	fmt.Println(easy.OrangesRotting(src))
-	src = [][]int{
-		{2, 1, 1},
-		{0, 1, 1},
-		{1, 0, 1},
-	}
-	fmt.Println(easy.OrangesRotting(src))
-	src = [][]int{
-		{1, 2},
-	}
-	fmt.Println(easy.OrangesRotting(src))
-	src = [][]int{
-		{0},
-	}
-	fmt.Println(easy.OrangesRotting(src))
+	fmt.Printf("%v\n", easy.DistributeCandies(7, 4))
+	fmt.Printf("%v\n", easy.DistributeCandies(10, 3))
+	fmt.Printf("%v\n", easy.DistributeCandies(100, 1))
 }
